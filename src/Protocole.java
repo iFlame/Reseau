c64db863bdb9c7f19167f61d62d4c6c8bc02081c
Quentin CORNEVIN <cornevin@polytech.unice.fr>
2014-11-10 16:18:47 +0100

    communication client / server ok, mais pas de protocole mis en place

---
+++ src/Protocole.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Quentin on 10/11/14.
+ */
+public class Protocole {
+
+
+
+
+    public void traiter(Answer a) {
+
+    }
+}
--- src/TCPClient.java
+++ src/TCPClient.java
@@ -3,19 +3,27 @@
  */
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TCPClient {
 
     public static void main(String argv[]) throws Exception  {
 
         Socket clientSocket = new Socket("localhost", 6789);
 
+
         Request request = new Request(10,"test", "test");
+
+
+        List<Request> requestList = new ArrayList<Request>();
 
         ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
         outToServer.writeObject(request);
         ObjectInputStream inFromServer = new ObjectInputStream((clientSocket.getInputStream()));
         Answer answer = (Answer) inFromServer.readObject();
-        System.out.print(answer);
+
+
 
         clientSocket.close();
     }
